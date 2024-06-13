031617604b55e2736320b361249d03fb3bfde49a
Intensio <Alexander0aki@gmail.com>
2024-06-13 03:55:12 +0300

    ui logic improved. Simple operations calculation example added

--- EasyCalc/EasyCalc/ViewModels/MainPageViewModel.cs
+++ EasyCalc/EasyCalc/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,10 +17,61 @@ namespace EasyCalc
         [ObservableProperty]
         private string _resultDisplay = string.Empty;
 
-        public MainPageViewModel() 
+
+        [RelayCommand]
+        public void HandleButtonPress(string buttonText)
         {
-            ExpressionDisplay = "4 + 5";
-            ResultDisplay = "9";
+            if (buttonText == "AC")
+            {
+                ExpressionDisplay = string.Empty;
+                ResultDisplay = string.Empty;
+            }
+
+            else if (buttonText == "=")
+            {
+                EvaluateExpression();
+            }
+
+            else if (int.TryParse(buttonText, out var _) || buttonText == "%" || buttonText == ".")
+            {
+                var ch = buttonText[0];
+                ExpressionDisplay += ch;
+            }
+            else if (buttonText == "DEL")
+            {
+                if (!string.IsNullOrEmpty(ExpressionDisplay))
+                {
+                    ExpressionDisplay = ExpressionDisplay.Substring(0, ExpressionDisplay.Length - 1);
+                }
+            }
+            else if (buttonText == "+" || buttonText == "-" || buttonText == "×" || buttonText == "/")
+            {
+                // Check if ExpressionDisplay already ends with an operation symbol
+                if (ExpressionDisplay.Length > 0 && "+-*//".Contains(ExpressionDisplay[ExpressionDisplay.Length - 1]))
+                {
+                    // Remove the last character from ExpressionDisplay
+                    var builder = new StringBuilder(ExpressionDisplay);
+                    builder.Remove(builder.Length - 1, 1);
+                    ExpressionDisplay = builder.ToString();
+                }
+
+                // Append the operation symbol to ExpressionDisplay
+                ExpressionDisplay += buttonText;
+            }
+        }
+
+        private void EvaluateExpression()
+        {
+            try
+            {
+                double result = Convert.ToDouble(new DataTable().Compute(ExpressionDisplay, null));
+                ResultDisplay = result.ToString();
+            }
+            catch
+            {
+                ResultDisplay = "Format error";
+                return;
+            }
         }
     }
 }
