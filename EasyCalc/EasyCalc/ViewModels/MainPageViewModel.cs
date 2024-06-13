70a25b9f30d9049e2c963b70651f0296ccd2baa6
Intensio <Alexander0aki@gmail.com>
2024-06-13 03:24:06 +0300

    initial calculator simplified interface realization

---
+++ EasyCalc/EasyCalc/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCalc
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _expressionDisplay = string.Empty;
+
+        [ObservableProperty]
+        private string _resultDisplay = string.Empty;
+
+        public MainPageViewModel() 
+        {
+            ExpressionDisplay = "4 + 5";
+            ResultDisplay = "9";
+        }
+    }
+}
